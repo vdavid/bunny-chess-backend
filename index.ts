25f9e4f1c0fd8273af373542ccc54b5788b5f1fa
David Veszelovszki <veszelovszki@gmail.com>
2022-04-26 00:33:47 +0200

    Implement simple chess backend

--- index.ts
+++ index.ts
@@ -1,12 +1,37 @@
 // ESM
 import Fastify from 'fastify'
+import fastifyCors from 'fastify-cors'
 
-const fastify = Fastify({
-    logger: true
+const fastify = Fastify({})
+
+/* Server start marks game start */
+const serverStartDate = new Date()
+const aBitAfterServerStartDate = new Date(serverStartDate.getTime() + 30 * 1000)
+
+fastify.register(fastifyCors, {
+    origin: '*'
 })
+const boards: {
+    [key: string]: {
+        fen: string,
+        lightElapsedMs: number,
+        darkElapsedMs: number,
+        lastUpdateDateTime: number,
+    }
+} = {}
 
-fastify.get('/', async (request, reply) => {
-    return { hello: 'world' }
+fastify.get('/', async (request) => {
+    const body = request.query as { id: string, fen: string, lightElapsedMs: string, darkElapsedMs: string, currentDateTime: string }
+    if (body?.id && body?.fen) {
+        boards[body.id] = {
+            fen: body.fen,
+            lightElapsedMs: parseInt(body?.lightElapsedMs),
+            darkElapsedMs: parseInt(body?.darkElapsedMs),
+            lastUpdateDateTime: parseInt(body?.currentDateTime)
+        }
+    }
+
+    return { boards, startDateTime: aBitAfterServerStartDate.getTime() }
 })
 
 const start = async () => {
