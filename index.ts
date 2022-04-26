60cfa79e7aa9558c08c0b374be0abe42c26587a7
David Veszelovszki <veszelovszki@gmail.com>
2022-04-26 09:46:14 +0200

    Change the interface a bit and adjust the API

--- index.ts
+++ index.ts
@@ -1,45 +1,36 @@
-// ESM
 import Fastify from 'fastify'
 import fastifyCors from 'fastify-cors'
 
+/* Create server and allow everyone to call it */
 const fastify = Fastify({})
+fastify.register(fastifyCors, { origin: '*' })
 
-/* Server start marks game start */
-const serverStartDate = new Date()
-const aBitAfterServerStartDate = new Date(serverStartDate.getTime() + 30 * 1000)
-
-fastify.register(fastifyCors, {
-    origin: '*'
-})
+/* Set up the store */
 const boards: {
     [key: string]: {
         fen: string,
         lightElapsedMs: number,
         darkElapsedMs: number,
-        lastUpdateDateTime: number,
+        lastUpdateDateTime: string,
+        gameStartDateTime: string,
     }
 } = {}
 
+/* Bind request handler */
 fastify.get('/', async (request) => {
     const body = request.query as { id: string, fen: string, lightElapsedMs: string, darkElapsedMs: string, currentDateTime: string }
-    if (body?.id && body?.fen) {
+    if (body && body.id && body.fen) {
         boards[body.id] = {
             fen: body.fen,
-            lightElapsedMs: parseInt(body?.lightElapsedMs),
-            darkElapsedMs: parseInt(body?.darkElapsedMs),
-            lastUpdateDateTime: parseInt(body?.currentDateTime)
+            lightElapsedMs: parseInt(body.lightElapsedMs) >= 0 ? parseInt(body.lightElapsedMs) : (boards[body.id]?.lightElapsedMs || 0),
+            darkElapsedMs: parseInt(body.darkElapsedMs) >= 0 ? parseInt(body.darkElapsedMs) : (boards[body.id]?.darkElapsedMs || 0),
+            lastUpdateDateTime: body.currentDateTime || '',
+            gameStartDateTime: boards[body.id]?.gameStartDateTime || body.currentDateTime
         }
     }
 
-    return { boards, startDateTime: aBitAfterServerStartDate.getTime() }
+    return boards
 })
 
-const start = async () => {
-    try {
-        await fastify.listen(2000)
-    } catch (err) {
-        fastify.log.error(err)
-        process.exit(1)
-    }
-}
-start()
+/* Start the server */
+fastify.listen(2000).then(() => {})
